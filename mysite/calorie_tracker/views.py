385b95e55ba63b53eca1e95a82855dd7295b0fd2
ShubhenduADutta <shubhendu@flexli.in>
2023-11-30 17:50:01 +0530

    Create a view for home page

--- mysite/calorie_tracker/views.py
+++ mysite/calorie_tracker/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from .models import Food
 # Create your views here.
+
+
+def index(request):
+    foods = Food.objects.all()
+    return render(request, 'calorie_tracker/index.html', {'foods': foods})
