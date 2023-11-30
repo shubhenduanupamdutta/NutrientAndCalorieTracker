f48fc8b03002110a468f7d0253a9d896b3e659e4
ShubhenduADutta <shubhendu@flexli.in>
2023-11-30 18:24:49 +0530

    Take action on post request by User
    Take action (store user and food) in database when user sends a post
    request with food. Also change context to  send food consumed by user.

--- mysite/calorie_tracker/views.py
+++ mysite/calorie_tracker/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
-from .models import Food
+from .models import Consume, Food
 # Create your views here.
 
 
 def index(request):
+    if request.method == 'POST':
+        food_consumed = request.POST.get('food_consumed')
+        consumed = Food.objects.get(name=food_consumed)
+        user = request.user
+        consume = Consume(user=user, food_consumed=consumed)
+        consume.save()
+
     foods = Food.objects.all()
-    return render(request, 'calorie_tracker/index.html', {'foods': foods})
+    consumed_food = Consume.objects.filter(user=request.user)
+    context = {"foods": foods, "consumed_food": consumed_food}
+    return render(request, 'calorie_tracker/index.html', context)
