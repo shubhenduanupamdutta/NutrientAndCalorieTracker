40c43645956e913fc2bc0e0ce24132d40f5f6eb8
ShubhenduADutta <shubhendu@flexli.in>
2023-11-30 17:33:56 +0530

    Create a Food model and ran migrations

--- mysite/calorie_tracker/models.py
+++ mysite/calorie_tracker/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Food(models.Model):
+    name = models.CharField(max_length=100)
+    carbs = models.FloatField()
+    protein = models.FloatField()
+    fat = models.FloatField()
+    calorie = models.IntegerField()
+
+    def __str__(self):
+        return self.name + ' - ' + str(self.calorie) + ' calories'
